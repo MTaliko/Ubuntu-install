f6693b0caa90e25e8fbe8d46a25a8a9f2f1b5a88
13MTlko <161132690+MTaliko@users.noreply.github.com>
2024-05-18 23:06:42 -0300

    Update install-for-termux.sh

--- Scripts/Termux/install-for-termux.sh
+++ Scripts/Termux/install-for-termux.sh
@@ -5,14 +5,12 @@ pkg update -y
 termux-setup-storage
 pkg upgrade -y
 pkg install x11-repo -y
-pkg install tur-repo wget git nano vim-gtk -y
+pkg install tur-repo wget proot git nano vim-gtk -y
 pkg install openssh openssl-tool curl -y
 pkg install php python python2 python3 -y
 ssh-keygen -t ed25519 -C "chr.str159@gmail.com"
 cat /data/data/com.termux/files/home/.ssh/id_ed25519.pub
 mkdir gitprojects
-cd gitprojects
 git config --global user.email "chr.str159@gmail.com"
 git config --global user.name "13MTaliko"
 git config --global core.editor "nano"
-git clone git@github.com:MTaliko/Ubuntu-install.github
